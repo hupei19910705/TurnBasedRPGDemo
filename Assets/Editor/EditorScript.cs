c0f359aa084ad36954987b1a309aeadf9f665a73
hupei <hupei_1991@163.com>
2020-01-30 11:10:00 +0800

    拓展编辑器

---
+++ Assets/Editor/EditorScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEditor;
+using UnityEngine;
+
+public class EditorScript
+{
+    [MenuItem("Assets/MyTools/LogAssetName", true, 1)]
+    static bool IsValidate()
+    {
+        return string.Equals(Selection.activeObject.name, "Test");
+    }
+
+    [MenuItem("Assets/MyTools/LogAssetName", false, 1)]
+    static void LogAssetName()
+    {
+        Debug.Log(Selection.activeObject.name);
+    }
+
+    [MenuItem("Assets/Create/MyCreate/Cube",false,1)]
+    static void CreateCube()
+    {
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+
+    [MenuItem("Root/Test1", false, 1)]
+    static void CreateTest1()
+    {
+        var menuPath = "Root/Test1";
+        bool mChecked = Menu.GetChecked(menuPath);
+        Menu.SetChecked(menuPath, !mChecked);
+    }
+
+    [MenuItem("CONTEXT/Transform/MyContext")]
+    static void MyContext(MenuCommand command)
+    {
+        Debug.Log(command.context.name);
+    }
+}
+
+[CustomEditor(typeof(Camera))]
+public class EditorCamera : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("拓展按钮")) { }
+        base.OnInspectorGUI();
+    }
+}
---
+++ Assets/EditorGizmos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditorGizmos : MonoBehaviour
+{
+    private void OnDrawGizmosSelected()
+    {
+        //Gizmos.color = Color.red;
+        //Gizmos.DrawLine(transform.position, Vector3.one);
+        //Gizmos.DrawCube(Vector3.one, Vector3.one);
+    }
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.DrawSphere(transform.position, 1);
+    }
+}

160b550ed762d936619b3f1ff701576769b06baf
hupei <hupei_1991@163.com>
2020-02-20 14:51:24 +0800

    增加游戏摇杆预制体

---
+++ Assets/Scripts/GameUtility/GameRocker.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GameRocker : ScrollRect
+{
+    private float _radius = 0f;
+
+    protected override void Start()
+    {
+        base.Start();
+        _radius = (transform as RectTransform).sizeDelta.x * 0.5f;
+    }
+
+    public override void OnDrag(PointerEventData eventData)
+    {
+        base.OnDrag(eventData);
+        var contentPos = content.anchoredPosition;
+        if (contentPos.magnitude > _radius)
+        {
+            contentPos = contentPos.normalized * _radius;
+            SetContentAnchoredPosition(contentPos);
+        }
+    }
+}
